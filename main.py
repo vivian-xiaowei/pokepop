b036bbd0a00abbb46c698a749239f518ecf14afb
vivian-xiaowei <102755468+vivian-xiaowei@users.noreply.github.com>
2022-06-16 08:55:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,55 @@
 import pygame.display
 from pygame.time import wait
 from gameplay import *
-
-
-map1_animation = []
-map2_animation = []
-map3_animation = []
-
-for i in range(1, 9):
-    map3_animation.append(transform.scale(load("cube" + str(i) + ".png"), (100, 103)))
-for i in range(1, 5):
-    map1_animation.append(transform.scale(load("leaf" + str(i) + ".png"), (85, 100)))
-
-animations = [map1_animation, map2_animation, map3_animation]
+from setup import *
+import sys
 
 
 def choose_map():
     global map
-    map1 = pygame.draw.polygon(window, (0, 0, 0), [(0, 0), (500, 0), (498, 351), (0, 695), (0, 0)], 1)
-    map2 = pygame.draw.polygon(window, (0, 0, 0), [(500, 0), (1000, 0), (1000, 690), (498, 351), (500, 0)], 1)
-    map3 = pygame.draw.polygon(window, (0, 0, 0),
-                               [(498, 351), (0, 695), (0, 750), (1000, 750), (1000, 690), (498, 351)], 1)
-    maps = [map1, map2, map3]
-    for i in range(3, 0, -1):
-        window.blit(load("choose maps/" + str(i) + ".png"), (0, 0))
+    pygame.draw.polygon(window, red, [(0, 0), (500, 0), (498, 351), (0, 695), (0, 0)])
+    pygame.draw.polygon(window, blue, [(500, 0), (1000, 0), (1000, 690), (498, 351), (500, 0)])
+    pygame.draw.polygon(window, black, [(498, 351), (0, 695), (0, 750), (1000, 750), (1000, 690), (498, 351)])
 
-    pygame.display.update()
-    if map3.collidepoint(pygame.mouse.get_pos()):
-        print(3)
-    # if map2.collidepoint(pygame.mouse.get_pos()):
-    #     print(2)
-    # if map1.collidepoint(pygame.mouse.get_pos()):
-    #     print(1)
+    mouse = pygame.mouse.get_pos()
+
     if pygame.mouse.get_pressed()[0]:
-        pos = pygame.mouse.get_pos()
-        for i in range(3):
-            if maps[i].collidepoint(pos):
-                map = i
-                break
+        if colourCollision(red, mouse):
+            map = 0
+        if colourCollision(blue, mouse):
+            map = 1
+        if colourCollision(black, mouse):
+            map = 2
         wait(500)
         choose_level()
         blackout()
-        game(map, level)
-        freeze()
+        game(map, 5)
+        aftergame()
+
+    for i in range(3, 0, -1):
+        window.blit(load("choose maps/" + str(i) + ".png"), (0, 0))
+    pygame.display.update()
+
+
+def colourCollision(colour, mouse):
+    if window.get_at(mouse) == colour:
+        return True
+    return False
 
 
 def choose_level():
     global level
     animation = animations[map]
-    count = -1 * len(animation)/60
+    count = 0
+    transition = True
     while True:
-        if pygame.mouse.get_pressed()[0] and count != -1 * len(animation)/60:
+        if pygame.mouse.get_pressed()[0] and not transition:
             break
+        transition = False
         window.fill((255, 255, 255))
         clock.tick(60)
         count += len(animation)/60
-        if int(count) == len(animation):
+        if int(count) > len(animation) - 1:
             count = 0
         window.blit(animation[int(count)], (0, 0))
 
@@ -72,13 +66,31 @@ def blackout():  # transition between choose map and game
     wait(1000)
 
 
-def freeze():
+def aftergame():
     wait(3000)
+
+
+def starting():
+    window.blit(load("start.png"), (0, 0))
+    pygame.display.update()
+    wait(5000)
 
 
 if __name__ == "__main__":
     map = 0
-    level = 0
+    level = 5
+
+    start = True
+    while start:
+        window.blit(load("start.png"), (0, 0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP:
+                start = False
+            elif event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
     while True:
         clock.tick(60)
         choose_map()
